5512e1f870cea05b8b945bba77f840b84c641601
NahAzevedo <azvdo.nayara@gmail.com>
2021-07-12 12:24:02 -0300

    start_project

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:agenda_de_contato/Helpers/Contact_helper.dart';
+import 'package:agenda_de_contato/ui/contact_page.dart';
+import 'package:agenda_de_contato/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
